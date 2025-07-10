00b4f3981fbf0b9a1005085a14ce0c183237b013
Katharine Prata (qris-uq) <uqkprat2@bunya1.rcc.uq.edu.au>
2025-07-10 11:07:14 +1000

    removing hpc param files from git and producing result files for recent hpc runs

--- scripts/slurm_reformat-gadma-results.sh
+++ scripts/slurm_reformat-gadma-results.sh
@@ -1,20 +1,33 @@
 #!/bin/bash --login
-#SBATCH --job-name="reformat-results"   	# job name
-#SBATCH --nodes=1               # use 1 node
-#SBATCH --ntasks-per-node=1     # use 1 for single and multi core jobs
-#SBATCH --cpus-per-task=3       # number of cores per job
-#SBATCH --mem=1G               # RAM per job given in megabytes (M), gigabytes (G), or terabytes (T)
-#SBATCH --time=24:00:00         # walltime
-#SBATCH --account=a_riginos     # group account name
-#SBATCH --partition=general     # queue name
-#SBATCH -o reformat_results-full_%A_%a.o # standard output
-#SBATCH -e reformat_results-full_%A_%a.e # standard error
+#SBATCH --job-name="2het_reformat-results"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=2G
+#SBATCH --time=24:00:00
+#SBATCH --account=a_riginos
+#SBATCH --partition=general
+#SBATCH -o reformat_results-proj_2het_%A_%a.o
+#SBATCH -e reformat_results-proj_2het_%A_%a.e
 
+
+# module load python/3.9.5-gcccore-10.3.0
 module load anaconda3/2023.09-0
-module load python/3.9.5-gcccore-10.3.0
+
 source $EBROOTANACONDA3/etc/profile.d/conda.sh
 conda activate gadma_env
 
+export PATH="/home/uqkprat2/.conda/envs/gadma_env/bin:$PATH"
+
+echo "Conda envs:"
+conda info --envs
+echo "Python path:"
+which python
+echo "Python version:"
+python --version
+echo "Dadi location:"
+python -c "import dadi; print(dadi.__file__)"
+
 cd /scratch/user/uqkprat2/analysis/kp_dadi/scripts
 
-python reformat-gadma-results.py '../results/gadma/full' '../results/gadma_full_results_combined.txt'
+python reformat-gadma-results_custom.py '../results/gadma/proj_2het' '../results/gadma_proj_2het_results_combined5.txt' 'custom_model_2p_2het.py' 0.8
