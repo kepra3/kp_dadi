10a4a00fba8c79c934bc63a9244fea79c6cbca3c
Katharine Prata (qris-uq) <uqkprat2@bunya2.rcc.uq.edu.au>
2025-07-17 13:38:45 +1000

    updated conf

--- scripts/slurm_confidence-intervals.sh
+++ scripts/slurm_confidence-intervals.sh
@@ -1,14 +1,14 @@
 #!/bin/bash --login
-#SBATCH --job-name="confidence_g2-g4"
+#SBATCH --job-name="confidence_g3-g4"
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=8G
-#SBATCH --time=3:00:00
+#SBATCH --time=10:00:00
 #SBATCH --account=a_riginos
 #SBATCH --partition=general
-#SBATCH -o confidence-g2-g4_%A_%a.o
-#SBATCH -e confidence-g2-g4_%A_%a.e
+#SBATCH -o confidence-g3-g4_%A_%a.o
+#SBATCH -e confidence-g3-g4_%A_%a.e
 
 
 # module load python/3.9.5-gcccore-10.3.0 ##### SBATCH --qos=debug
@@ -36,7 +36,7 @@ opt=$2
 echo "Population pair is: $pop"
 echo "Optimised params are: $opt"
 
-for i in 0.0001; do
+for i in 0.00001 0.000001; do
     echo "Running confidence intervals for 1het with epsilon ${i} for ${pop}"
     python confidence_intervals.py "../data/fs/${pop}_projected0.8.fs" "../data/fs/projected_${pop}/" \
         GIM  1het ${i} -o ${opt}
